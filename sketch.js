06a116abf6e1294815ee79679d250c8e27f0e1e7
Emily Butler <ekb21a@fsu.edu>
2023-09-14 17:06:04 -0400

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
 function setup() {
   createCanvas(400, 400);
+  fill("#F2B5D4")
+  noStroke()
 }
 
 function draw() {
-  background(125);
+  background("#7BDFF2");
+  rect(300, 100, 100, 100)
+  rect(300, 300, 100, 100)
+  rect(200, 200, 100, 100)
+  rect(100, 300, 100, 100)
+  rect(0, 200, 100, 100)
+  rect(100, 100, 100, 100)
+  rect(0, 0, 100, 100)
+  rect(200, 0, 100, 100)
+
 }
